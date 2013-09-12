1fa3321460e51950a5c71061ba2f587b006a1a26
rolies106 <rolies106@gmail.com>
2013-09-13 02:35:10 +0700

    Add sample get user profile

--- index.php
+++ index.php
@@ -11,23 +11,69 @@
 <body>
     <div id="main">
         <div id="titleBar">
-            <h2>YouTube API sample in PHP</h2>
+            <h2>Contoh penggunaan API Youtube dengan PHP</h2>
         </div>
         <?php if (!authenticated()) : ?>
             <p>
-                You are not connected, please <a href="<?php echo generateSubTokenUrl(); ?>">Click Here</a>
-                to connect to your Youtube account
+                Anda belum terhubung dengan Youtube silahkan <a href="<?php echo generateSubTokenUrl(); ?>">Klik disini</a>
+                untuk menghubungkan aplikasi ini dengan account Youtube anda
             </p>
         <?php else : ?>
             <p>
-                You are connected with token :
+                Anda sudah terhubung, dan token anda adalah :
                 <pre><?php echo $_SESSION['sessionToken']; ?></pre>                
             </p>
 
-            <p>
-                Sample use with current token :
+            <hr>
 
-                <pre>Under Construction</pre>
+            <h1>Contoh penggunaan token</h1>
+
+            <h3>User Profile</h3>
+
+            <p>
+                Target Url : <code>https://gdata.youtube.com/feeds/api/users/default</code>
+            </p>
+
+            <p>
+                Pada saat melakukan <code>GET</code> request ke url tersebut anda harus menyertakan token yang anda dapat pada saat menghubungkan aplikasi, contoh kode :
+
+                <pre>
+                    $httpClient = Zend_Gdata_AuthSub::getHttpClient(TOKEN_SESSION);
+
+                    // lihat di https://developers.google.com/gdata/docs/developers-guide atau
+                    // http://stackoverflow.com/questions/10704299/zend-gdata-calendar-api-returns-version-3-0-is-not-supported
+                    $httpClient->setHeaders('GData-Version', '2.0');
+
+                    $youtube = new Zend_Gdata_YouTube($httpClient, APPLICATION_ID, CLIENT_ID, DEVELOPER_KEY);
+
+                    // Variabel $profile akan menyimpan semua informasi profile dari user yang terhubung
+                    // dalam bentuk object class Zend_Gdata_YouTube_UserProfileEntry
+                    $profile = $youtube->getUserProfile();
+                </pre>
+
+                Hasil yang didapat :
+
+                <pre>
+<?php
+                    $httpClient = Zend_Gdata_AuthSub::getHttpClient($_SESSION['sessionToken']);
+                    $httpClient->setHeaders('GData-Version', '2.0');
+                    $youtube = new Zend_Gdata_YouTube($httpClient, APP_ID, CLIENT_ID, DEV_KEY);
+
+                    // Variabel $profile akan menyimpan semua informasi profile dari user yang terhubung
+                    $profile = $youtube->getUserProfile('default');
+
+                    echo '// $profile->getUsername();<br/>';
+                    echo $profile->getUsername();
+
+                    echo '<br/><br/>';
+                    echo '// $profile->getFirstName();<br/>';
+                    echo $profile->getFirstName();
+
+                    echo '<br/><br/>';
+                    echo '// $profile->getLastName();<br/>';
+                    echo $profile->getLastName();
+                    ?>
+                </pre>
             </p>
         <?php endif; ?>
     </div>
